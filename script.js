2029a3bc8c70009f58a15083d2e4e19831846d97
Michauuek <michal.rzemieniuk@gmail.com>
2024-05-05 18:51:23 +0200

    Added auto fetching area code

--- script.js
+++ script.js
@@ -10,6 +10,46 @@ function handleClick() {
     clicksInfo.innerText = clickCount;
 }
 
+async function getAreaCodeByCity() {
+
+    const city = document.getElementById('city').value;
+    const street = document.getElementById('street').value;
+
+
+    if (!city || !street) {
+        console.error('Brak wymaganych danych');
+        return;
+    }
+
+
+    const header = {'Application': 'application/json'};
+    const apiUrl = `http://kodpocztowy.intami.pl/city/${city}/street/${street}`;
+
+    try {
+        // const response = await fetch(apiUrl, method: "GET", header);
+        const response = await fetch(apiUrl, {
+            method: "GET",
+            headers: {"Accept": "application/json"}
+        })
+
+        const data = await response.json();
+        const code = data[0];
+
+
+        if (!code) {
+            console.error('Brak danych');
+            return;
+        }
+
+        console.log(code);
+        document.getElementById('zipCode').value = code
+        console.log(`Set data ${code}`);
+    } catch (error) {
+        console.error('Wystąpił błąd:', error);
+    }
+}
+
+
 async function fetchAndFillCountries() {
     try {
         const response = await fetch('https://restcountries.com/v3.1/all');
@@ -30,6 +70,9 @@ function getCountryByIP() {
         .then(data => {
             const country = data.country;
             // TODO inject country to form and call getCountryCode(country) function
+            console.log(`Country 1 ${country}`);
+            countryInput.value = country;
+            getCountryCode(country);
         })
         .catch(error => {
             console.error('Błąd pobierania danych z serwera GeoJS:', error);
@@ -37,6 +80,7 @@ function getCountryByIP() {
 }
 
 function getCountryCode(countryName) {
+    console.log(`Country name ${countryName}`)
     const apiUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
 
     fetch(apiUrl)
@@ -49,6 +93,8 @@ function getCountryCode(countryName) {
     .then(data => {        
         const countryCode = data[0].idd.root + data[0].idd.suffixes.join("")
         // TODO inject countryCode to form
+        console.log(`Country code ${countryCode}`);
+        document.getElementById('countryCode').value = countryCode;
     })
     .catch(error => {
         console.error('Wystąpił błąd:', error);
@@ -59,6 +105,43 @@ function getCountryCode(countryName) {
 (() => {
     // nasłuchiwania na zdarzenie kliknięcia myszką
     document.addEventListener('click', handleClick);
-
+    getCountryByIP();
     fetchAndFillCountries();
 })()
+
+
+document.getElementById('street').addEventListener('blur', function() {
+    console.log('Input field lost focus:', this.value);
+    // Call your function here if the field is not empty
+    if (this.value.trim() !== '' && document.getElementById('city').value.trim() !== '') {
+        getAreaCodeByCity();
+    }
+});
+
+// function checkPassword(input) {
+//     const passwordRegex = /^[A-Za-z]\w{7,99}$/;
+//     const inputField = document.getElementById('exampleInputPassword1');
+//     if (input.match(passwordRegex)) {
+//         console.log("valid");
+//         inputField.classList.remove('is-invalid');
+//     } else {
+//         console.log("invalid");
+//         inputField.classList.add('is-invalid');
+//     }
+// }
+
+document.getElementById('exampleInputPassword1').addEventListener('blur', function() {
+    console.log('Input field lost focus:', this.value);
+    if (this.value.trim() !== '') {
+        // checkPassword(this.value);
+    }
+});
+
+// document.getElementById('submitBtn').addEventListener('submit', function(event) {
+//     const passwordInput = document.getElementById('exampleInputPassword1');
+//     const isPasswordValid = checkPassword(passwordInput.value);
+//     if (!isPasswordValid) {
+//         // event.preventDefault();
+//         console.log('Form submission prevented due to invalid password.');
+//     }
+// });
